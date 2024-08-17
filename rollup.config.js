d7041289f2b1f09e632044562db46e6f6a020ac3
liangpengyv <me@laoliang.ink>
2024-08-17 11:18:29 +0800

    build: 拆分打包

--- rollup.config.js
+++ rollup.config.js
@@ -2,13 +2,23 @@ import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
 import json from '@rollup/plugin-json'
 
-export default {
-  input: 'src/generate.js',
-  output: [
-    {
-      file: 'dist/bundle.js',
+const plugins = [resolve(), commonjs(), json()]
+
+export default [
+  {
+    input: 'src/fetch.js',
+    output: {
+      file: 'dist/fetch.bundle.js',
       format: 'esm',
     },
-  ],
-  plugins: [resolve(), commonjs(), json()],
-}
+    plugins,
+  },
+  {
+    input: 'src/generate.js',
+    output: {
+      file: 'dist/generate.bundle.js',
+      format: 'esm',
+    },
+    plugins,
+  },
+]
